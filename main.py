10a38b43563487a59f434158962ee1a9c8bef3df
Kassuhn <Rhyankassuhn@gmail.com>
2026-04-03 19:11:53 -0700

    Added optional --verbose flag to output metadata if true

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
 
     parser = argparse.ArgumentParser(description="User input parser")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -26,9 +27,13 @@ def main():
     if response.usage_metadata is None:
         raise RuntimeError("Failed API request")
     
-    print(f"Prompt tokens:  {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens:  {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(response.text)
+    else:
+        print(response.text)
 
 
 
